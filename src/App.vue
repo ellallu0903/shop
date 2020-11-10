c978ee709d263c56a1b91a24bc28cd3fc682db70
ellallu0903 <luyizhen93@gmail.com>
2020-11-10 10:51:45 +0800

    完成商品頁

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template lang="pug">
 #app
-  #nav
-    router-link(to='/') Home
-    | &nbsp;|&nbsp;
-    router-link(to='/about') About
-  router-view
+  b-container
+    b-row
+      b-col.text-center(cols="12")
+        #nav
+          router-link(to='/') Home
+          | &nbsp;|&nbsp;
+          router-link(to='/about') About
+      b-col(cols="12")
+        router-view
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,41 @@
 <template lang="pug">
-#home
+  #home
+    b-row
+      b-col(cols="12" md="6" lg="3" v-for="(item,index) in items" :key="index")
+        b-card(:title="item.name" :img-src="item.img" img-top :sub-title="'$'+item.price")
+          b-button(variant="primary")
+            font-awesome-icon(:icon="['fas','shopping-cart']")
+            | &npsp; 加入購物車
 </template>
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  data () {
+    return {
+      items: [
+        {
+          img: 'https://e.ecimg.tw/items/DHAY3K1900AF2ZB/000001_1604571681.jpg',
+          name: 'Microsoft 微軟 3吋10代i5輕薄觸控筆電',
+          price: 22888
+        },
+        {
+          img: 'https://d.ecimg.tw/items/DYAJ93A900AXPNV/000001_1603176778.jpg',
+          name: 'Apple iPhone 12 (128G)-黑色',
+          price: 28500
+        },
+        {
+          img: 'https://d.ecimg.tw/items/DYAN70A900AUPA5/000001_1599102543.jpg',
+          name: '小米手環 5',
+          price: 845
+        },
+        {
+          img: 'https://e.ecimg.tw/items/DYAJE01900AXAFZ/000001_1603678779.jpg',
+          name: 'AirPods Pro',
+          price: 6090
+        }
+      ]
+    }
+  }
 }
 </script>
